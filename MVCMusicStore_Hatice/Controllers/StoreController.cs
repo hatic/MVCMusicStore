2474046cc9c39baca759180c2501da2a84dab654
hatic <hatice.caglar3@gmail.com>
2017-01-01 14:22:01 +0300

    MVC Music Store
    mvcmusicstore.codeplex.com kaynağından yararlanılarak geliştirilmiştir

---
+++ MVCMusicStore_Hatice/Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStore_Hatice.Models;
+using System.Data.Entity;
+
+
+namespace MVCMusicStore_Hatice.Controllers
+{
+    public class StoreController : Controller
+    {
+
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+            return View(genres);
+        }
+        //We’re using the HttpUtility.HtmlEncode utility method to sanitize the user input. 
+        //This prevents users from injecting Javascript into our View with a link 
+        //like /Store/Browse?Genre=<script>window.location=’http://hackersite.com’</script>.
+        //zararlı kod çalışmasının önüne geçmek için  HttpUtility.HtmlEncode metodunu kulladık
+        public ActionResult Browse(string genre)
+        {// Retrieve Genre and its Associated Albums from database
+            var genreModel = storeDB.Genres.Include("Albums")
+            .Single(g => g.Name == genre);
+            return View(genreModel);
+        }
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+            return View(album);
+        }
+
+    }
+}
---
+++ MVCMusicStore_Hatice/Models/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MVCMusicStore_Hatice.Models
+{
+    [Bind(Exclude = "AlbumId")]
+    public class Album
+    {
+        [ScaffoldColumn(false)]
+        public int AlbumId { get; set; }
+        [DisplayName("Albüm Kategori")]
+        public int GenreId { get; set; }
+        [DisplayName("Sanatçı")]
+        public int ArtistId { set; get; }
+        [Required(ErrorMessage = "Albüm adı gereklidir")]
+        [StringLength(160)]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Fiyat alanı boş geçilemez")]
+        [Range(0.01, 100.00,
+        ErrorMessage = "Fiyat 0.01 ve 100.00 aralığında olmalı")]
+        public decimal Price { get; set; }
+        [DisplayName("Albüm URL")]
+        [StringLength(1024)]
+        public string AlbumArtUrl { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
+    }
+}
